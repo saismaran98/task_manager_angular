7c7a79f93a3fbc9738a464c10d3b09944ab40a44
SaismaranHota <saismaran.hota@ibm.com>
2024-10-31 01:58:53 +0530

    string interpolation example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
 
 
 // @Component is a decorator: this add some metadata to this class. 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent], //if you want to update other compoent inside this component. 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+
+const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length)
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  /* String Interpolation {{any_public_property_of_the_class}}  */
+  selectedUser =  DUMMY_USERS[randomIndex] // declaring this vlaue selectedUser with out cosnt or let you are providing its value to the template i.e html which is called data binding. 
+
+
+}
