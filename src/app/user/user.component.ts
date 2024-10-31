1fb4f79a3d95e6827ba9b6772792cadc0dfffb42
SaismaranHota <saismaran.hota@ibm.com>
2024-11-01 01:07:58 +0530

    added comment on signal approach, not very widely used

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input} from '@angular/core';
+import { Component, computed, Input, input} from '@angular/core';
 
 // @Input: decorator, input: special function
 @Component({
@@ -9,14 +9,21 @@ import { Component, Input, input} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  /* signal is used very less so we will use approach without signal */
+
+  
   // @Input({required:true}) avatar !: string; //with require:true use !: that assure there is a value. this @Input make it settable from outside so we can set it from diff place, !: this will be set by outside value and not currently set. 
   // @Input({required:true}) name !: string;
-  avatar = input.required<string>(); //using signal
+  avatar = input.required<string>(); //using signal, these input are read only not to write. 
   name = input.required<string>();
 
-  get imagePath(){
-    return 'assets/users/'+this.avatar(); //using signal make var a function call.
-  }
+  // when to use readsignal when you want to notify dom change.
+  // get imagePath(){
+  //   return 'assets/users/'+this.avatar(); //using signal make var a function call.
+  // }
+  imagePath = computed( ()=> {
+    return 'assets/users/'+this.avatar();
+  })
   onSelectedUser(){
    
   }
