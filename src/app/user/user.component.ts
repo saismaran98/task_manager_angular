a29c4dbab24800328604e6e6bfbebef72081ae50
SaismaranHota <saismaran.hota@ibm.com>
2024-11-01 00:56:55 +0530

    required nd optional fields

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,7 +9,7 @@ import { Component, Input} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() avatar !: string; // this @Input make it settable from outside so we can set it from diff place, !: this will be set by outside value and not currently set. 
+  @Input({required:true}) avatar !: string; //with require:true use !: that assure there is a value. this @Input make it settable from outside so we can set it from diff place, !: this will be set by outside value and not currently set. 
   @Input() name !: string;
 
   get imagePath(){
