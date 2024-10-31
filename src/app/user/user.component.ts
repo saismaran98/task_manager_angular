2fd69be441079fa0fa35e54c9cfbf1f2d6aef327
SaismaranHota <saismaran.hota@ibm.com>
2024-11-01 01:04:49 +0530

    using signal to input and modify values instead of decorator @input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, input} from '@angular/core';
 
-
+// @Input: decorator, input: special function
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,11 +9,13 @@ import { Component, Input} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required:true}) avatar !: string; //with require:true use !: that assure there is a value. this @Input make it settable from outside so we can set it from diff place, !: this will be set by outside value and not currently set. 
-  @Input() name !: string;
+  // @Input({required:true}) avatar !: string; //with require:true use !: that assure there is a value. this @Input make it settable from outside so we can set it from diff place, !: this will be set by outside value and not currently set. 
+  // @Input({required:true}) name !: string;
+  avatar = input.required<string>(); //using signal
+  name = input.required<string>();
 
   get imagePath(){
-    return 'assets/users/'+this.avatar;
+    return 'assets/users/'+this.avatar(); //using signal make var a function call.
   }
   onSelectedUser(){
    
