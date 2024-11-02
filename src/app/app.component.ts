85205f5659dcf0ace28500f89d26186a180e6cc1
SaismaranHota <saismaran.hota@ibm.com>
2024-11-02 17:02:33 +0530

    added task component and passed input username to it!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
+import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { CommonModule } from '@angular/common';
+import { TasksComponent } from './tasks/tasks.component';
 
-// @Component is a decorator: this add some metadata to this class. 
+// @Component is a decorator: this add some metadata to this class.
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, CommonModule], //if you want to update other compoent inside this component. 
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    UserComponent,
+    CommonModule,
+    TasksComponent,
+  ], //if you want to update other compoent inside this component.
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-onSelectedUser(id:string) {
-  console.log('selected user id is: '+id)
-}
-  title = 'angular-project-udemy';
   users = DUMMY_USERS;
+  selectedUserId = DUMMY_USERS[0].id;
 
+  get selectedUser(){
+    return this.users.find(user => user.id === this.selectedUserId)!; //this.selectedUserId)!, ! if we dont find user we are returning undefined and task-component does not want undefined, we are telling tytpescript that this will not happen by using !
+  }
+  onSelectedUser(id: string) {
+    this.selectedUserId = id;
+  }
+  title = 'angular-project-udemy';
 }
-
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -17,22 +17,19 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) id!: string;
-  // @Input({ required: true }) avatar!: string; //with require:true use !: that assure there is a value. this @Input make it settable from outside so we can set it from diff place, !: this will be set by outside value and not currently set.
-  // @Input({ required: true }) name!: string;
-  //@Output() showTaskForClickedUser = new EventEmitter();
- 
-  // input method create a signal not output, if you are using input funciton, just use output without any decorator.
-  id = input.required<string>();
-  avatar= input.required<string>();
-  name= input.required<string>();
-
-  showTaskForClickedUser = output<string>(); // when using output we need to specify which kind of data will be emited output() will throw error so use output<string>();
-
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string; //with require:true use !: that assure there is a value. this @Input make it settable from outside so we can set it from diff place, !: this will be set by outside value and not currently set.
+  @Input({ required: true }) name!: string;
+  @Output() userComponentEmmiter = new EventEmitter<string>(); //commonly used rather than output
+  
   get imagePath() {
     return 'assets/users/' + this.avatar; //using signal make var a function call.
   }
   onSelectedUser() {
-    this.showTaskForClickedUser.emit(this.id());
+    this.userComponentEmmiter.emit(this.id);
   }
+  sendUserNames(){
+    this.userComponentEmmiter.emit(this.name);
+  }
+
 }
